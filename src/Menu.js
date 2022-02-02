eb36c5d17987931b4b9ec32efeb93159e0472251
Franco Vanney <francoavanney@gmail.com>
2022-02-02 13:12:13 -0300

    ul en vez de sections

--- src/Menu.js
+++ src/Menu.js
@@ -1,21 +1,28 @@
 import React from "react";
+import {Table} from 'react-bootstrap'
 
 const Menu = ({ items }) => {
   return (
-    <div className="section-center">
+    <div className="container">
       {items.map((item) => {
         const { id, title, img, desc, price } = item;
         return (
-          <article key={id} className="menu-item">
-            {/* <img src={img} alt={title} className="photo" /> */}
-            <div className="item-info">
-              <header>
-                <h4>{title}</h4>
-                <h4 className="price">${price}</h4>
-              </header>
-              <p className="item-text">{desc}</p>
-            </div>
-          </article>
+          <Table>
+            <tbody >
+              <tr key={id} className="{/*menu-item*/}">
+                  <td>
+                      {/* <img src={img} alt={title} className="photo" /> */}
+                      <div className="item-info">
+                        <header>
+                          <h4>{title}</h4>
+                          <h4 className="price">${price}</h4>
+                        </header>
+                          <p className="item-text">{desc}</p>
+                      </div>
+                  </td>
+              </tr>
+            </tbody>
+          </Table>
         );
       })}
     </div>
