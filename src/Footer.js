00947f02d2a3d46d008d98120dc48418874a3ea1
Franco Vanney <francoavanney@gmail.com>
2022-02-03 13:47:47 -0300

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,46 @@
+import logoPampa from './assets/img/LogoPampaWhite.png'
+import logoVinilo from './assets/img/logo_vinilo.jpg'
 
 const Footer = () => {
-  return <div 
-    className="footer 
-    text-center 
-    d-flex 
-    justify-content-center 
-    align-items-center">
-        <span>© Pampa Code - {(new Date().getFullYear())}</span>
-    </div>;
+  return (
+          <div className="footer">
+            <div className="container">
+              <div className="row">
+                  {/*<!-- Footer Location
+                  <div className="col-lg-4 mb-5 mb-lg-0">
+                      <h4 className="text-uppercase mb-4">Location</h4>
+                      <p className="lead mb-0">
+                          2215 John Daniel Drive
+                          <br />
+                          Clark, MO 65243
+                      </p>
+                  </div>
+                  -->*/}
+                  {/*<!-- Footer Social Icons-->*/}
+                  <div className="col-lg-6 mb-5 mb-lg-0">
+                      <a href="https://www.pampacode.com" target={"_blank"}>
+                          <img src={logoPampa} alt="Pampacode" id='logo-footer-pampa' />
+                      </a>
+                      <p className="lead mb-0">
+                      © Todos los derechos reservados - {(new Date().getFullYear())}.
+                      </p>
+                      <a className="btn btn-outline-light btn-social mx-1 mt-3" href="https://www.instagram.com/pampacode" target={"_blank"}><i className="fab fa-fw fa-instagram"></i></a>
+                      <a className="btn btn-outline-light btn-social mx-1 mt-3" href="https://www.pampacode.com" target={"_blank"}><i className="fas fa-fw fa-globe"></i></a>
+                      <a className="btn btn-outline-light btn-social mx-1 mt-3" href={"mailto:info@pampacode.com"}><i className="fas fa-envelope"></i></a>
+                  </div>
+                  {/*<!-- Footer About Text-->*/}
+                  <div className="col-lg-6">
+                      <img src={logoVinilo} alt="Vinilo Bar" id='logo-vinilo' />
+                      <p className="lead mb-0">
+                          Junín, Bs As, Argentina.
+                      </p>
+                      <a className="btn btn-outline-light btn-social mx-1 mt-3" href="https://www.instagram.com/vinilo.bar/" target={"_blank"}><i className="fab fa-fw fa-instagram"></i></a>
+                      <a className="btn btn-outline-light btn-social mx-1 mt-3" href="https://www.facebook.com/ViniloBarClub" target={"_blank"}><i className="fab fa-facebook-f"></i></a>
+                  </div>
+              </div>
+            </div>
+          </div>
+  )
 };
 
 export default Footer;
